df1384b2f5f32ed234b6ac8735c763b4c9832b16
Twichher <vladickflorin@gmail.com>
2024-10-26 14:27:59 +0300

    second

--- web/views.py
+++ web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Hello world!')
--- web_project/settings.py
+++ web_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'web',
     'django.contrib.postgres',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
