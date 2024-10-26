638641e3c0c1878b63ae853aa285cfaf6e69b329
justamix <fmmf20@yandex.ru>
2024-10-26 13:53:50 +0300

    models4

--- web/models.py
+++ web/models.py
@@ -1,5 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.hashers import make_password
+
+class CustomUser(models.Model):
+    username = models.CharField(unique=True, max_length=150)
+    email = models.EmailField(("email адрес"), unique=True)
+    password = models.CharField(max_length=50, verbose_name="Пароль")    
+    is_superuser = models.BooleanField(default=False, verbose_name="Является ли пользователь админом?")
+
+    board_games = models.FloatField(default=0, verbose_name="Отношение к настольным играм")
+    arts = models.FloatField(default=0, verbose_name="Отношение к исскуству")
+    sport = models.FloatField(default=0, verbose_name="Отношение к спорту")
+    nature = models.FloatField(default=0, verbose_name="Отношение к природе")
+    food = models.FloatField(default=0, verbose_name="Отношение к еде")
+
+    duration = models.FloatField(default=0, verbose_name="Предпочтительное время мероприятий")
+    capacity = models.FloatField(default=0, verbose_name="Предпочтительное кол-во мест")
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+
+    def __str__(self):
+        return f'{self.username}'
+
+    class Meta:
+        managed = True
+        db_table = 'custom_user'
 
 
 class Category(models.Model):
