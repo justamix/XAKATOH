be7d3de664d9aa25b441975b7e5598cb50b2a193
justamix <fmmf20@yandex.ru>
2024-10-26 12:33:25 +0300

    models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField()
+
+    class Meta:
+        managed =True
+        db_table = 'category'
+
+class event(models.Model):
+    date = models.DateTimeField()
+    playground = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    address = models.CharField(max_length=100)
+    is_open = models.BooleanField(default=True)
+    duration_in_minutes = models.IntegerField(default=60)
+    capacity = models.IntegerField()
+    category = models.ForeignKey()
+
+    class Meta:
+        managed = True 
+        db_table = 'event'
+
+
+class EventUser(models.Model):
+    event = 
+
